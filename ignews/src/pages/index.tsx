7d490fd29451ca00322f24e0d8fc5a6b46b44f31
Luan Simões <luaan.carlos@hotmail.com>
2022-05-10 23:21:51 -0300

    Finalizando Introducao ao NextJs

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number,
+  }
+}
+
+export default function Home(props: HomeProps) {
   return (
     <>
       <Head>
@@ -14,12 +24,41 @@ export default function Home() {
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>Get access to all the publication <br />
-            <span>for $9.90 month</span>
+            <span>for {props.product.amount} month</span>
           </p>
+          <SubscribeButton priceId={props.product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   )
+}
+
+//GetServerSideProps === Sempre atualiza
+//GetStacticProps === Atualiza em um determinado tempo
+
+export const getServerSideProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Ky4DaBVvAjyeoqcv5S0BMcK',
+  /*
+  Para mostras mais dados :
+  {
+    expand: ['product'] traz os dados do produto
+  } */
+  );
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return{
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24 hours
+  };
 }
