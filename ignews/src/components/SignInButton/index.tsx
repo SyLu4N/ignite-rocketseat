75fb8b874b0e8a3a1c4ef0f564fb36a80ee07eb1
Luan Simões <85505258+SyLu4N@users.noreply.github.com>
2022-05-12 17:04:51 -0300

    Conection FaunaDB

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,19 +1,20 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SingInButton() {
-  const isUserLoggendIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggendIn ? (
-    <button type='button' className={styles.singInButton}>
-      <FaGithub color='#04d361'/>
-      Luan Simões
-      <FiX color='#737380' className={styles.closeIcon}/>
+  return session ? (
+    <button type='button' className={styles.singInButton} onClick={() => signOut()}>
+      <FaGithub color='#04d361' />
+      {session.user.name}
+      <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
-    <button type='button' className={styles.singInButton}>
-      <FaGithub color='#eba417'/>
+    <button type='button' className={styles.singInButton} onClick={() => signIn('github')}>
+      <FaGithub color='#eba417' />
       Sign in with Github
     </button>
   );
--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -5,11 +5,10 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton(props: SubscribeButtonProps) {
-  console.log(props.priceId);
   return (
     <button
-    type='button'
-    className={styles.subscribeButton}
+      type='button'
+      className={styles.subscribeButton}
     >
       Subscribe now
     </button>
