4dc186c7ba3a15380530a1c51f69d16097c07d16
Luan Simões <85505258+SyLu4N@users.noreply.github.com>
2022-05-13 17:43:05 -0300

    Create stripe

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,9 @@
+import { signIn, useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripeJs';
+import { BiLoaderAlt } from 'react-icons/bi'
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,12 +11,43 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton(props: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubscribe() {
+    setLoading(true);
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    //Create at checkout session
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      alert(error.message);
+    }
+  }
+
   return (
     <button
       type='button'
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
+      disabled={loading}
     >
-      Subscribe now
+      <div className='containerTeste'>
+        Subscribe now
+        {loading ? <span className='loading'><BiLoaderAlt /></span> : ''}
+      </div>
     </button>
   );
 }
