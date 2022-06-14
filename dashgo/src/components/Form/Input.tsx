bc75799e24cea3531eaa21155be603d303d2aaea
Luan Simões <85505258+SyLu4N@users.noreply.github.com>
2022-06-14 17:54:18 -0300

    retomando

---
+++ dashgo/src/components/Form/Input.tsx
@@ -0,0 +1,29 @@
+import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInoutProps } from '@chakra-ui/react';
+
+interface InputProps extends ChakraInoutProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps): JSX.Element {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+      <ChakraInput
+        name={name}
+        id={name}
+        type="email"
+        focusBorderColor='pink.500'
+        bg='gray.900'
+        variant='filled'
+        _hover={{
+          bg: 'gray.900'
+        }}
+        size='lg'
+        {...rest}
+      />
+    </FormControl>
+
+  );
+}
