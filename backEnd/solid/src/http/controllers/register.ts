acbee253caf3b54d6bbf109ce6c3653b5550d5bc
Luan Simões <85505258+SyLu4N@users.noreply.github.com>
2023-11-10 17:51:55 -0300

    Estruturando pastas

---
+++ backEnd/solid/src/http/controllers/register.ts
@@ -0,0 +1,23 @@
+import { prisma } from '@/lib/prisma';
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { z } from 'zod';
+import { hash } from 'bcryptjs';
+import { registerUseCase } from '@/use-cases/register';
+
+export async function register(req: FastifyRequest, res: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(req.body);
+
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (error) {
+    return res.status(409).send();
+  }
+
+  return res.status(201).send();
+}
