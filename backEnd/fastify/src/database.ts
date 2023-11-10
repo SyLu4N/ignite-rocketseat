047be2995bbcb3a74cb58cdc9463d1a371ac385d
Luan Simões <luaan.carlos@hotmail.com>
2023-11-09 22:08:08 -0300

    finalizando back end

--- backEnd/fastify/src/database.ts
+++ backEnd/fastify/src/database.ts
@@ -1,14 +1,15 @@
 import { knex as setupKnex, Knex } from "knex";
 import { env } from "./env";
 
+const { DATABASE_CLIENT, DATABASE_URL } = env;
+
 export const configKnex: Knex.Config = {
-  client: "sqlite",
+  client: DATABASE_CLIENT,
 
   useNullAsDefault: true,
 
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  connection:
+    DATABASE_CLIENT === "sqlite" ? { filename: DATABASE_URL } : DATABASE_URL,
 
   migrations: {
     extension: "ts",
