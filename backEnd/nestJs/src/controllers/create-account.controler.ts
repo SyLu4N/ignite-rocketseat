99714a36d570aadcf0778231478f9b5a93dc5955
Luan Simões <85505258+SyLu4N@users.noreply.github.com>
2023-11-24 17:15:37 -0300

    tatt

---
+++ backEnd/nestJs/src/controllers/create-account.controler.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Post,
+  Body,
+  ConflictException,
+  UsePipes,
+} from '@nestjs/common';
+import { hash } from 'bcryptjs';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipes';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { z } from 'zod';
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(3),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = body;
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    const passwordHash = await hash(password, 8);
+
+    if (userWithSameEmail) throw new ConflictException('E-mail já cadastrado');
+
+    await this.prisma.user.create({
+      data: { name, email, password: passwordHash },
+    });
+  }
+}
